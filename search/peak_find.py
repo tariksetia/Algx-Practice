191da3768e4272c7fb30ed2fff9496ace39e128f
tariksetia <setia.tarik@gmail.com>
2018-09-07 13:31:28 +0530

    ttron

--- search/peak_find.py
+++ search/peak_find.py
@@ -5,19 +5,19 @@ def peak_find(data):
         mid = (lo+hi)//2
         if mid > hi-1 : return hi #When the array is sorted but not rotated mid = hi so mid+1 doesn't exist
 
-        if data[mid] < data[mid-1]:
+        if data[mid] > data[mid-1]:
             hi = mid-1
-        elif data[mid] < data[mid+1]:
+        elif data[mid] > data[mid+1]:
             lo = mid+1
         else:
             return mid
 
 
 if __name__ == '__main__':
-    a=[6,7,8,9,1,3,4,5,6]
-    b=[5,6,7,8,9,1]
-    c=[3,4,5,6,7,1,2]
-    d=[1,2,3,4,5,6,7]
+    a = [6,7,8,9,1,3,4,5,6]
+    b = [5,6,7,8,9,1]
+    c = [3,4,5,6,7,1,2]
+    d = [1,2,3,4,5,6,7]
 
     print(peak_find(a))
     print(peak_find(b))
